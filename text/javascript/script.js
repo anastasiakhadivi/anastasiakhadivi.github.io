68c0bdf1a21c67ee9155806ee1c047e6fce3c54a
anastasiakhadivi <khada121@newschool.edu>
2017-02-07 20:15:03 -0500

    adding .css in query

--- text/javascript/script.js
+++ text/javascript/script.js
@@ -2,8 +2,10 @@ $(function() {
     // alert("hey");
     $( ".li_drag" ).draggable();
     $( ".droppable" ).droppable({
-      drop: function( event) {
+      drop: function(event) {
+      	// $(".draggable").css("margin-top", "30px");
         // $(".li_drag").css("font-family", "Franklin Gothic Heavy");
+        // $(".droppable").css("height","350px");
         console.log("Dropped!");
       }
     });
