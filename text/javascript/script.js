508b2e1ce49865abd33fe0c7c710168da44d7c01
anastasiakhadivi <khada121@newschool.edu>
2017-02-14 20:42:09 -0500

    moving up word bank to top, full poems on bottom of pages

--- text/javascript/script.js
+++ text/javascript/script.js
@@ -1,12 +1,9 @@
+console.log("i'm working");
 $(function() {
     // alert("hey");
-    $( ".li_drag" ).draggable();
-    $( ".droppable" ).droppable({
+    $( ".li_drag" ).draggable({
+    // $( ".droppable" ).droppable({
       drop: function(event) {
-      	// $(".draggable").css("margin-top", "30px");
-        // $(".li_drag").css("font-family", "Franklin Gothic Heavy");
-        // $(".droppable").css("height","350px");
-        console.log("Dropped!");
       }
     });
   } );
