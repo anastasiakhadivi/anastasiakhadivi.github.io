0ff5b578516123a28dac6832f313e1b83e110126
anastasiakhadivi <khada121@newschool.edu>
2017-02-14 21:41:03 -0500

    original poems appear when button clicked... start out as hidden

--- text/javascript/script.js
+++ text/javascript/script.js
@@ -1,9 +1,13 @@
 console.log("i'm working");
 $(function() {
     // alert("hey");
-    $( ".li_drag" ).draggable({
+    $( ".li_drag, .frost, .mckay" ).draggable({
     // $( ".droppable" ).droppable({
       drop: function(event) {
       }
     });
+    $(".poem_container").hide();
+    $(".li_drag").click(function(){
+        $(".poem_container").show();
+    })
   } );
