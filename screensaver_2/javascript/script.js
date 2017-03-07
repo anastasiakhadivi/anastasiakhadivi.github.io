96871490847dc3e2cf66061ee9c70ca2e030f4df
anastasiakhadivi <khada121@newschool.edu>
2017-03-07 12:56:02 -0500

    changed number of circles

--- screensaver_2/javascript/script.js
+++ screensaver_2/javascript/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	console.log("working")
-	var numOfCircles = 4;
+	var numOfCircles = 6;
 	for (var i = 0; i < numOfCircles; i+i+1) {
 		$(".circle_container").append("<div class='circle'></div>");
+		console.log(numOfCircles);
 	}
 });
