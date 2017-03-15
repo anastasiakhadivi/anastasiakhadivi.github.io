71496fdcbee6a46e685f2c9b01fac883edf8d712
anastasiakhadivi <khada121@newschool.edu>
2017-03-15 10:33:17 -0400

    moved all script from 3.js to script.js

--- screensaver/javascript/script.js
+++ screensaver/javascript/script.js
@@ -1,173 +1,190 @@
 $(document).ready(function(){
+	
+	var numOfCircle = 4;
+	for (var i=0; i<numOfCircle; i=i+1)
+		$(".circle_container").append("<div id='circle-" + i + "' class='circle'></div>");
 
-				var numOfCircle = 4;
+	circle1();
 
+		// circle1();
+		function circle1(){
+			$("#circle-0").addClass("show");
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width': '31vw',
+				'height':'31vw'
+			},1000, 'linear')
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear', circle2);
+			console.log("circle 1 only");
+		}
 
-
-				for (var i=0; i<numOfCircle; i=i+1)
-					$(".circle_container").append("<div id='circle-" + i + "' class='circle'></div>");
-
-				$(".circle:nth-child(2),.circle:nth-child(3), .circle:nth-child(4)").hide();
-
-			
-				animateCircles().done(function(){
-					executeAfterCircle1();
-				})
-				animateCircles2().done(function(){
-					executeAfterCircle2();
-				})
-
-				animateCircles3().done(function(){
-					executeAfterCircle3();
-				})
-				animateCircles4().done(function(){
-					executeAfterCircle4();
-				})
-
-
-				function executeAfterCircle1(){
-					console.log("adding second circle");
-					$(".circle:nth-child(2)").show();
-					animateCircles2();
-				}
-
-				function executeAfterCircle2(){
-					console.log("adding third circle");
-					$(".circle:nth-child(3)").show();
-					animateCircles3();
-				}
-				function executeAfterCircle3(){
-					console.log("adding fourth circle");
-					$(".circle:nth-child(4)").show();
-					animateCircles4();				
-				}
-				function executeAfterCircle4(){
-					console.log("cycle done!");
-					$(".circle:nth-child(2), .circle:nth-child(3), .circle:nth-child(4)").hide();
-					animateCircles();
-				}
-
-				function animateCircles(){
-					console.log('circle 1 only');
-					var deferred = $.Deferred();
-					$(".circle:first-child").animate({
-						'width' : '30vw',
-						'height': '30vw'
-					},4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000,'linear',function(){
-						deferred.resolve();
-					})
-
-					return deferred.promise();
-					// console.log(animateCircles);
-				}
-				function animateCircles2(){
-					var deferred = $.Deferred();
-					$(".circle:nth-child(2)").animate({
-						'width' : '40vw',
-						'height': '40vw'
-					}, 4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000, 'linear');
-					$(".circle:first-child").animate({
-						'width' : '35vw',
-						'height': '35vw'
-					},4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000,'linear', function(){
-						deferred.resolve();
-					})
-					return deferred.promise();
-				}
-				function animateCircles3(){
-					var deferred = $.Deferred();
-					$(".circle:nth-child(3)").animate({
-						'width' : '44vw',
-						'height': '44vw'
-					}, 4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000, 'linear');
-					$(".circle:nth-child(2)").animate({
-						'width' : '40vw',
-						'height': '40vw'
-					}, 4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000, 'linear');
-					$(".circle:first-child").animate({
-						'width' : '35vw',
-						'height': '35vw'
-					},4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000,'linear', function(){
-						deferred.resolve();
-					})
-					return deferred.promise();
-				}
-				function animateCircles4(){
-
-					var deferred = $.Deferred();
-					$(".circle:nth-child(4)").animate({
-						'width' : '50vw',
-						'height': '50vw'
-					}, 4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000, 'linear');
-					$(".circle:nth-child(3)").animate({
-						'width' : '44vw',
-						'height': '44vw'
-					}, 4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000, 'linear');
-					$(".circle:nth-child(2)").animate({
-						'width' : '40vw',
-						'height': '40vw'
-					}, 4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000, 'linear');
-					$(".circle:first-child").animate({
-						'width' : '35vw',
-						'height': '35vw'
-					},4000, 'linear')
-					.delay(7000)
-					.animate({
-						'height' : '1vw',
-						'width' :'1vw'
-					}, 8000,'linear',function(){
-						deferred.resolve();
-					})
-					return deferred.promise();
-				}
-
-				
-
-
-
-			});
+		function circle2(){
+			// $("#circle-0").addClass("show");
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-1").addClass("show");
+			$("#circle-1").animate({
+				'width' : '35vw',
+				'height': '35vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear', circle3);
+			console.log("circle 1 & 2 only");
+		}
+		function circle3(){
+			// $("#circle-0").addClass("show");
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			// $("#circle-1").addClass("show");
+			$("#circle-1").animate({
+				'width' : '35vw',
+				'height': '35vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-2").addClass("show");
+			$("#circle-2").animate({
+				'width' : '40vw',
+				'height': '40vw'
+			}, 2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height': '1vw'
+			}, 6000, 'linear', circle4);
+			console.log("circle 1,2,3!");
+		}
+		function circle4(){
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-1").animate({
+				'width' : '35vw',
+				'height': '35vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-2").animate({
+				'width' : '40vw',
+				'height': '40vw'
+			}, 2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height': '1vw'
+			}, 6000, 'linear');
+			$("#circle-3").addClass("show");
+			$("#circle-3").animate({
+				'width' : '44vw',
+				'height': '44vw'
+			}, 2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height': '1vw'
+			}, 6000, 'linear', circleA);
+			console.log("circle 4!");
+		}
+		function circleA(){
+			$("#circle-3").removeClass("show");
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-1").animate({
+				'width' : '35vw',
+				'height': '35vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-2").animate({
+				'width' : '40vw',
+				'height': '40vw'
+			}, 2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height': '1vw'
+			}, 6000, 'linear', circleB);
+			console.log("removed circle 4");
+		}
+		function circleB(){
+			$("#circle-2").removeClass("show");
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear');
+			$("#circle-1").animate({
+				'width' : '35vw',
+				'height': '35vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear', circleC);
+			console.log("removed 3rd circle");
+		}
+		function circleC(){
+			$("#circle-1").removeClass("show");
+			$("#circle-0").animate({
+				'width' : '30vw',
+				'height': '30vw'
+			},2000, 'linear')
+			.delay(4000)
+			.animate({
+				'width' : '1vw',
+				'height':'1vw'
+			},6000, 'linear', circle2);
+			console.log("removed 2nd circle");
+		}
+})
