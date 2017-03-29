cb408fe7ec354eef5da8078205b740ec12c388cd
anastasiakhadivi <khada121@newschool.edu>
2017-03-29 12:05:42 -0400

    API ABC assignment

---
+++ apiabc/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var url = "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=c1ac9baab6fc4360912cd5b4b02716a0";
+
+	$getJSON(url, function(data){	
+
+	})
+})
