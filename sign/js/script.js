11a6fd04442e8e7975013fd18b9132db0bec9ebc
anastasiakhadivi <khada121@newschool.edu>
2017-04-17 22:28:42 -0400

    uploading tenitive final

--- sign/js/script.js
+++ sign/js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
 	// alert("hey");
-	var width = $(".frame_1").innerWidth();
-	// alert(width);
-	var height = (260 * width) /420;
-	$(".frame_1").innerHeight(height);
+	$("h1").draggable();
 })
