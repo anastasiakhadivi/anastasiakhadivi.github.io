169f2b92ca2d24aab9183a25830a1a471c282809
anastasiakhadivi <khada121@newschool.edu>
2017-04-18 15:38:05 -0400

    audio poppet working

--- sign/js/script.js
+++ sign/js/script.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	// alert("hey");
 	$("h1").draggable();
 
-
+	// function audioWindow(){
+	// 	window.open("", "", "width=300, height:100" );
+	// }
+	// audioWindow();
 })
