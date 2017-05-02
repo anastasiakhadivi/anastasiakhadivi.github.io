5ccc753ead0255f85ced6fc0a9a184937396b79d
anastasiakhadivi <khada121@newschool.edu>
2017-05-02 16:34:12 -0400

    adding names of shapes(items)

--- collection/js/script.js
+++ collection/js/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
 	// alert("hey");
-	$(".name_reveal").hide();
+
+	$(".shoes_madden").hover(function(){
+		$(".shoes_madden_name").toggle();
+	})
+	$(".shoes_puma").hover(function(){
+		$(".shoes_puma_name").toggle();
+	})
+	$(".toliet_paper").hover(function(){
+		$(".toliet_paper_name").toggle();
+	})
+	$(".umbrella").hover(function(){
+		$(".umbrella_name").toggle();
+	})
+	$(".soap").hover(function(){
+		$(".soap_name").toggle();
+	})
+	$(".beer").hover(function(){
+		$(".beer_name").toggle();
+	})
+	$(".cream").hover(function(){
+		$(".cream_name").toggle();
+	})
+	$(".eggs").hover(function(){
+		$(".eggs_name").toggle();
+	})
+	$(".cream_cheese").hover(function(){
+		$(".cream_cheese_name").toggle();
+	})
+	$(".beef").hover(function(){
+		$(".beef_name").toggle();
+	})
 })
